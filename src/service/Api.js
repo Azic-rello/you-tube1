4de0c62f4e74f459f9a7b04bd86886c1955857e5
Azic-rello <github1akk2009@gmail.com>
2025-12-05 15:26:56 +0500

    update 100

--- src/service/Api.js
+++ src/service/Api.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 const options = {
   method: "GET",
   params: {
-    maxResults: "50",
+    maxResults: "100",
   },
   headers: {
     // "x-rapidapi-key": "e5576a2210msh6cb07c786d822a4p1f9699jsn68c273acd936",
